78190b68761ba0654c8a0237725da8307fb15786
marxus87 <ms.koval.87@gmail.com>
2016-03-21 19:36:57 +0200

    7 edited and augmented

--- Project7/js/script.js
+++ Project7/js/script.js
@@ -38,12 +38,12 @@ var method = {
 			var pageBuilder = document.querySelectorAll('form');
 			var element = document.createElement('h2');
 			element.classList.add('question');
-			var j = i * 2;
-			element.innerHTML = data.ulQuestions[j]; 
+			var j = i * 2; // Массив ulQuestions имеет 6 элементов, "Вопрос"" и в массиве "Варианты ответа", и так три раза. Благодаря этой переменной я перепрыгиваю через массивы.
+			element.innerHTML = data.ulQuestions[j];
 			pageBuilder[0].appendChild(element);
 			element = document.createElement('ul');
 			pageBuilder[0].appendChild(element);
-			for (var y=0; y<data.ulQuestions[1].ulAnswers.length; y++) {
+			for (var y=0; y<data.ulQuestions[j+1].ulAnswers.length; y++) { // Благодаря этой переменной я могу задавать разное количество вариантов ответов в базе данных.
 				var pageBuilder = document.querySelectorAll('ul');
 				var element = document.createElement('li');
 				element.classList.add('answer');
@@ -51,17 +51,17 @@ var method = {
 
 				pageBuilder = document.querySelectorAll('li');
 				element = document.createElement('label');
-				pageBuilder[i + j + y].appendChild(element);
+				pageBuilder[i + j + y].appendChild(element); // Без j и с другими работать не будет
 
 				pageBuilder = document.querySelectorAll('label');
 				element = document.createElement('input');
 				element.setAttribute('type', 'checkbox');
-				element.setAttribute('name', 'choice'+ ++i);
-				i = --i;
-				pageBuilder[i + j + y].appendChild(element);
+				element.setAttribute('name', 'choice'+ ++i); // При создании имени старался создать разные name-названия, начиная от 1.
+				i = --i; // Чтобы не создавать ещё одну переменную e=i+1 я допустил такой код. Могу поменять, но так проще.
+				pageBuilder[i + j + y].appendChild(element); // И здесь тоже.
 				element = document.createElement('span');
 				element.innerHTML = data.ulQuestions[j + 1].ulAnswers[y];
-				pageBuilder[i + j + y].appendChild(element);				
+				pageBuilder[i + j + y].appendChild(element); // И здесь.		
 			}
 		}
 	},
@@ -72,9 +72,10 @@ var method = {
 		element.setAttribute('type', 'submit');
 		pageBuilder.appendChild(element);
 	},
-	pageInit: function() {method.wrapper(),
-method.list(),
-method.submitButton()}
+	pageInit: function() {
+		method.wrapper(),
+		method.list(),
+		method.submitButton()}
 };
 
 method.pageInit();
