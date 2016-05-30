74d418de2f27e9a1fa878f57b31c37249e30c3de
marxus87 <ms.koval.87@gmail.com>
2016-05-30 19:26:26 +0300

    ready

---
+++ ProjectF/js/scriptTest.js
@@ -0,0 +1,16 @@
+var app = {
+	sum: function(b,c) {
+		return a=b+c;
+	},
+	sub: function(b,c) {
+		return a=b-c;
+	},
+	mul: function(b,c) {
+		return a=b*c;
+	},
+	dis: function(b,c) {
+		return a=b/c;
+	}
+}
+
+module.exports = app;
