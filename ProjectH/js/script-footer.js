d8c6050db9d87b2a42e24998021802bd3d26da35
Mark Koval <ms.koval.87@gmail.com>
2018-05-30 00:24:29 +0300

    Delete script-footer.js

--- ProjectH/js/script-footer.js
+++
@@ -1,9 +0,0 @@
-(function($){
-	$(function(){
-		var Height = document.querySelector('.footer').offsetHeight;
-	    setHeight = 'padding-bottom:'+Height+'px';
-	    document.querySelector('.wrapper').setAttribute('style',setHeight);
-	    setHeight = 'margin-top:-'+Height+'px';
-	    document.querySelector('.footer').setAttribute('style',setHeight);
-	});
-})(jQuery);
